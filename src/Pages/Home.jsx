ebc5c5abdb6bd23ce4082ca411353d862749f385
Guilherme <guilherme.maugustini@gmail.com>
2021-04-24 19:07:40 -0300

    feat: create page home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import Filter from '../Components/Filter/Filter';
+import Header from '../Components/Header/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Filter />
+    </>
+  );
+}
