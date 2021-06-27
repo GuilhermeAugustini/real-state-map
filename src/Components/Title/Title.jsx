e90ef507935efce301813f881af57ffeed5d9645
Guilherme Augustini <guilherme.maugustini@gmail.com>
2021-06-27 00:04:33 -0300

    feat: add props className

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,6 +1,6 @@
 import ProtoTypes from 'prop-types';
 
-export default function Title({ children, as }) {
+export default function Title({ children, as, className }) {
   const tags = {
     h1: 'h1',
     h2: 'h2',
@@ -10,14 +10,16 @@ export default function Title({ children, as }) {
     h6: 'h6',
   };
   const Component = tags?.[as] ?? 'h1';
-  return <Component>{children}</Component>;
+  return <Component className={className}>{children}</Component>;
 }
 
 Title.propTypes = {
   children: ProtoTypes.node.isRequired,
   as: ProtoTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  className: ProtoTypes.string,
 };
 
 Title.defaultProps = {
   as: 'h1',
+  className: '',
 };
