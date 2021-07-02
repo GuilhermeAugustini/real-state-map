fe88998eea3ae1862b38e7b587e4d88c2ced9fca
Guilherme Augustini <guilherme.maugustini@gmail.com>
2021-07-02 00:29:16 -0300

    feat: add props to scrollbar and height

--- src/Components/Overflow/Overflow.jsx
+++ src/Components/Overflow/Overflow.jsx
@@ -1,10 +1,28 @@
 import ProtoTypes from 'prop-types';
-import { overflow } from './Overflow.module.scss';
+import useClassNames from '../../Hooks/useClassNames';
+import { overflow, scroll, notScroll } from './Overflow.module.scss';
 
-export default function Overflow({ children }) {
-  return <div className={overflow}>{children}</div>;
+export default function Overflow({ children, notScrollbar, height, className }) {
+  const classes = useClassNames();
+  return (
+    <div
+      className={classes(overflow, notScrollbar ? notScroll : scroll, className)}
+      style={{ maxHeight: height }}
+    >
+      {children}
+    </div>
+  );
 }
 
 Overflow.propTypes = {
   children: ProtoTypes.node.isRequired,
+  notScrollbar: ProtoTypes.bool,
+  height: ProtoTypes.string,
+  className: ProtoTypes.string,
+};
+
+Overflow.defaultProps = {
+  notScrollbar: false,
+  height: '100vh',
+  className: null,
 };
