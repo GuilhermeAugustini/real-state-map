d76a66d0865c32fcf30b766b11effd0cedfd8f68
Guilherme Augustini <guilherme.maugustini@gmail.com>
2021-07-02 00:43:45 -0300

    feat: organize components within containers

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,46 @@
+import { useState, useCallback } from 'react';
 import Card from '../../Components/Card/Card';
 import CardGrid from '../../Components/Card/CardGrid/CardGrid';
+import Container from '../../Components/Container/Container';
 import Filter from '../../Components/Filter/Filter';
-import Portal from '../../Components/Portal/Portal';
-import Menu from '../../Components/Menu/Menu';
 import Header from '../../Components/Header/Header';
 import Map from '../../Components/Map/Map';
-import { container, containerFilter, overflow } from './Home.module.scss';
+import Menu from '../../Components/Menu/Menu';
+import Overflow from '../../Components/Overflow/Overflow';
+import Portal from '../../Components/Portal/Portal';
+import { container, containerFilter, containerOverflow, overflow } from './Home.module.scss';
 
+const initialState = {
+  grid: true,
+  menu: false,
+};
 export default function Home() {
+  const [state, setState] = useState(initialState);
+  const dispatchState = useCallback(
+    (newState) => setState((prev) => ({ ...prev, ...newState })),
+    []
+  );
   return (
     <>
-      <Portal>
-        <Menu />
-      </Portal>
+      {state.menu && (
+        <Portal>
+          <Menu setMenu={dispatchState} />
+        </Portal>
+      )}
+
       <Header />
-      <div className={container}>
-        <div className={containerFilter}>
-          <Filter />
-          <div className={overflow}>
-            <CardGrid />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </div>
-        </div>
+
+      <Container className={container}>
+        <Container className={containerFilter}>
+          <Filter setFilter={dispatchState} grid={state.grid} />
+          <Overflow height="85vh" className={overflow}>
+            <div className={containerOverflow} data-grid={state.grid}>
+              {Array.from({ length: 8 }).map(() => (state.grid ? <CardGrid /> : <Card />))}
+            </div>
+          </Overflow>
+        </Container>
         <Map />
-      </div>
+      </Container>
     </>
   );
 }
